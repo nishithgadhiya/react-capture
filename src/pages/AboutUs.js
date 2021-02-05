c98b3ffa6da4766014479f9253d6438b05681bac
Nishith Gadhiya <nishithgadhiya@gmail.com>
2021-02-05 11:56:51 +0530

    added services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,15 @@
 import React from "react";
 //Page components
 import AboutSection from "../compoents/AboutSection";
+import ServicesSection from "../compoents/ServicesSection";
 
 const ABoutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default ABoutUs;
