ba4df01f2d17adb8168028b1337d49832117b740
Nishith Gadhiya <nishithgadhiya@gmail.com>
2021-02-05 15:18:39 +0530

    added Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //Page components
 import AboutSection from "../compoents/AboutSection";
 import ServicesSection from "../compoents/ServicesSection";
+import FaqSection from "../compoents/FaqSection";
 
 const ABoutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />>
     </>
   );
 };
